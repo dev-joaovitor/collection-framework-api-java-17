0a6737fe769e649147404f0a93b152b37778003d
joaotorvitor <joaotorvitor1@gmail.com>
2023-09-04 21:27:45 -0300

    taskList tutorial implemented

--- src/main/java/list/BasicOperations/TaskList.java
+++ src/main/java/list/BasicOperations/TaskList.java
@@ -1,7 +1,53 @@
 package main.java.list.BasicOperations;
 
-public class TaskList {
-    public static void main(String[] args) {
+import java.lang.annotation.Target;
+import java.util.ArrayList;
+import java.util.List;
 
+public class TaskList {
+    private List<Task> taskList;
+
+    public TaskList() {
+        this.taskList = new ArrayList<>();
+    }
+
+    public void addTask(String description) {
+        taskList.add(new Task(description));
+    }
+
+    public void removeTask(String description) {
+        List<Task> taskToRemove = new ArrayList<>();
+
+        for (Task t: taskList){
+            if (t.getDescription().equalsIgnoreCase(description)) {
+                taskToRemove.add(t);
+            }
+        }
+        taskList.removeAll(taskToRemove);
+    }
+
+    public int getTaskListSize() {
+        return taskList.size();
+    }
+
+    public void getTasksDescription() {
+        System.out.println(taskList);
+    }
+
+    public static void main(String[] args) {
+        TaskList taskList = new TaskList();
+
+        System.out.println("The size of the task list is: " + taskList.getTaskListSize());
+
+        taskList.addTask("Task 1");
+        taskList.addTask("Task 1");
+        taskList.addTask("Task 2");
+
+        System.out.println("The size of the task list is: " + taskList.getTaskListSize());
+
+        taskList.removeTask("Task 1");
+        System.out.println("The size of the task list is: " + taskList.getTaskListSize());
+
+        taskList.getTasksDescription();
     }
 }
